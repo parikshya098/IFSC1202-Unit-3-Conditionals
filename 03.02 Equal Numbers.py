204de49dc5ce5bdd6be56f6b53efd2915c1381cf
parikshya098 <pbhandari1@ualr.edu>
2022-02-08 01:46:30 +0000

    Unit 3

--- 03.02 Equal Numbers.py
+++ 03.02 Equal Numbers.py
@@ -2,4 +2,13 @@ a = int(input('Enter first number: '))
 b = int(input('Enter second number: '))
 c = int(input('Enter third number: '))
 
+same_number=0
 
+if (a==b==c):
+    same_number = 3
+elif (a==b and a!=c or b==c and b!=a or a==c and a!=b ):
+    same_number = 2
+elif (a!=b and b!= c and a!=c):
+    same_number = 0
+
+print(same_number)
